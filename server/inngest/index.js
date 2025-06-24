cc074da5d3d5c560711451eb46b24a1c0bf7688e
elyse502 <elyseniyibizi502@gmail.com>
2025-06-24 18:38:21 +0200

    feat: Create inngest function to store user data in MongoDB dbs from clerk

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -3,5 +3,21 @@ import { Inngest } from "inngest";
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
-// Create an empty array where we'll export future Inngest functions
-export const functions = [];
+// Inngest Function to save user data to a database
+const syncUserCreation = inngest.createFunction(
+  { id: "sync-user-from-clerk" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userDate = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: first_name + " " + last_name,
+      image: image_url,
+    };
+    await User.create(userDate);
+  }
+);
+
+export const functions = [syncUserCreation];
