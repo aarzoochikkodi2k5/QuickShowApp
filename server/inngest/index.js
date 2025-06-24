498b9bc15359274ea26e168acb595d065b18f3d2
elyse502 <elyseniyibizi502@gmail.com>
2025-06-24 18:51:08 +0200

    feat: Implement inngest function to update user data from MongoDB dbs

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -30,4 +30,21 @@ const syncUserDeletion = inngest.createFunction(
   }
 );
 
-export const functions = [syncUserCreation, syncUserDeletion];
+// Inngest Function to update user data in database
+const syncUserUpdation = inngest.createFunction(
+  { id: "update-user-from-clerk" },
+  { event: "clerk/user.updated" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: first_name + " " + last_name,
+      image: image_url,
+    };
+    await User.findByIdAndUpdate(id, userData);
+  }
+);
+
+export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
