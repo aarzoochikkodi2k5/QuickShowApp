b168e438b90ef46bd7af5527aaf36c35d11982be
elyse502 <elyseniyibizi502@gmail.com>
2025-06-27 16:09:23 +0200

    feat: Create controller function to create new booking

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,3 +1,5 @@
+import e from "express";
+import Booking from "../models/Booking.js";
 import Show from "../models/Show.js";
 
 // Function to check availability of selected seats for a movie
@@ -14,5 +16,49 @@ const checkSeatsAvailability = async (showId, selectedSeats) => {
   } catch (error) {
     console.log(error.message);
     return false;
+  }
+};
+
+export const createBooking = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { showId, selectedSeats } = req.body;
+    const { origin } = req.headers;
+
+    // Check if the seat is available for the selected show
+    const isAvailable = await checkSeatsAvailability(showId, selectedSeats);
+
+    if (!isAvailable) {
+      return res.json({
+        success: false,
+        message: "Selected Seats are not available.",
+      });
+    }
+
+    // Get the show details
+    const showData = await Show.findById(showId).populate("movie");
+
+    // Create a new booking
+    const booking = await Booking.create({
+      user: userId,
+      show: showId,
+      amount: showData.showPrice * selectedSeats.length,
+      bookedSeats: selectedSeats,
+    });
+
+    selectedSeats.map((seat) => {
+      showData.occupiedSeats[seat] = userId;
+    });
+
+    showData.markModified("occupiedSeats");
+
+    await showData.save();
+
+    // Stripe Gateway Initialize
+
+    res.json({ success: true, message: "Booked successfully." });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
   }
 };
