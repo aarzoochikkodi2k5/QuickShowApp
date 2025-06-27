338580163f3eb407b25a0d299b40eeb206443cdd
elyse502 <elyseniyibizi502@gmail.com>
2025-06-27 17:11:05 +0200

    feat: Create controller function to get Admin's Dasboard Data

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,4 +1,32 @@
+import Booking from "../models/Booking.js";
+import Show from "../models/Show";
+import User from "../models/User";
+
 // API to check if user is an admin
 export const isAdmin = async (req, res) => {
   res.json({ success: true, isAdmin: true });
+};
+
+// API to get dashboard data
+export const getDashboardData = async (req, res) => {
+  try {
+    const bookings = await Booking.find({ isPayed: true });
+    const activeShows = await Show.find({
+      showDateTime: { $gte: new Date() },
+    }).populate("movie");
+
+    const totalUser = await User.countDocuments();
+
+    const dashboardData = {
+      totalBookings: bookings.length,
+      totalRevenue: bookings.reduce((acc, booking) => acc + booking.amount, 0),
+      activeShows,
+      totalUser,
+    };
+
+    res.json({ success: true, dashboardData });
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: error.message });
+  }
 };
