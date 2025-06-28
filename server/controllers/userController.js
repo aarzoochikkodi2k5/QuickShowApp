302fe9867764fad4f94e65619f1a86432f4a4106
elyse502 <elyseniyibizi502@gmail.com>
2025-06-28 09:19:12 +0200

    feat: Create controller function (API) to get the list of favorite movies

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,6 @@
 import { clerkClient } from "@clerk/express";
 import Booking from "../models/Booking.js";
+import Movie from "../models/Movie.js";
 
 // API Controller Function to Get User Bookings
 export const getUserBookings = async (req, res) => {
@@ -45,6 +46,22 @@ export const updateFavorite = async (req, res) => {
     });
 
     res.json({ success: true, message: "Favorite movies updated" });
+  } catch (error) {
+    console.error(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// API Controller Function to Get Favorite Movies from Clerk User Metadata
+export const getFavorites = async (req, res) => {
+  try {
+    const user = await clerkClient.users.getUser(req.auth().userId);
+    const favorites = user.privateMetadata.favorites;
+
+    // Getting movies from database
+    const movies = await Movie.find({ _id: { $in: favorites } });
+
+    res.json({ success: true, movies });
   } catch (error) {
     console.error(error.message);
     res.json({ success: false, message: error.message });
