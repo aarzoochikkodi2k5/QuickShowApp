b9c27393a49bcf3a8c85d41aa5d10257b8945609
aarzoochikkodi2k5 <aarzoochikkodi05@gmail.com>
2025-10-22 17:24:33 +0530

    Update Movies.jsx

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -5,7 +5,7 @@ import { useAppContext } from "../context/AppContext";
 const Movies = () => {
   const { shows } = useAppContext();
 
-  return shows.length > 0 ? (
+  return shows && shows.length > 0 ? (
     <div className="relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]">
       <BlurCircle top="150px" left="0" />
       <BlurCircle bottom="50px" right="50px" />
@@ -24,3 +24,4 @@ const Movies = () => {
 };
 
 export default Movies;
+
