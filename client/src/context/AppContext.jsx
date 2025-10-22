5e0b8da7c96e4aa39df1b0927ffe8a15a50d1fa3
aarzoochikkodi2k5 <aarzoochikkodi05@gmail.com>
2025-10-22 20:15:18 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -4,7 +4,8 @@ import { useAuth, useUser } from "@clerk/clerk-react";
 import { useLocation, useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
-axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+
 
 export const AppContext = createContext();
 
